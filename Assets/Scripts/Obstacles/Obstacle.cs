0b536b5c4f4e3be1badaa99433dd7e212e5a73e2
Joniel Ibasco <jlibasco@gmail.com>
2014-01-25 03:02:52 +0800

    Add support for player death.
    Assets/Scripts/GameController.cs

---
+++ Assets/Scripts/Obstacles/Obstacle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+	public SenseController.SenseType lethalFor;
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Obstacles/ScentObstacle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScentObstacle : Obstacle {
+
+	// Use this for initialization
+	void Start () {
+		lethalFor = SenseController.SenseType.Scent;
+	}
+
+	void Update () {
+		//if he enters trigger, kill player
+
+	}
+}
