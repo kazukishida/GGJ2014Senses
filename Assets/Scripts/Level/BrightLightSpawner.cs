a221a238d9ee1f40b7aedd9798794ca9a56c44ed
Joshua Castaneda <thejosh223@gmail.com>
2014-01-25 01:20:26 +0800

    Added light particle

---
+++ Assets/Scripts/Level/BrightLightSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrightLightSpawner : MonoBehaviour {
+
+	private float distanceInstantiate = 6f;
+	public GameObject lightQuad;
+
+	void Start () {
+		Vector3 scale = transform.localScale;
+		int numToInstantiate = (int) ((scale.x * scale.y * scale.z) / distanceInstantiate);
+		for (int i = 0; i < numToInstantiate; i++) {
+			GameObject g = Instantiate(lightQuad, transform.position + 
+			                           new Vector3(Random.Range(-0.5f, 0.5f) * scale.x, Random.Range(-0.5f, 0.5f) * scale.y, Random.Range(-0.5f, 0.5f) * scale.z), 
+			                           Quaternion.identity) as GameObject;
+		}
+	}
+	
+	void Update () {
+	
+	}
+}
