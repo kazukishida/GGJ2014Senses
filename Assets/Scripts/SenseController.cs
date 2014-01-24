10002f1f67e439da062292acbe0c4554945e7f8e
Nigel Rodriguez <anr.rod@gmail.com>
2014-01-24 15:50:10 +0000

    add SenseController

---
+++ Assets/Scripts/SenseController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SenseController : MonoBehaviour {
+	public GameObject SightGO;
+	public GameObject HearingGO;
+	public GameObject ScentGO;
+	public GameObject FeelingGO;
+	
+	public enum SenseType {
+		Sight, Hearing, Scent, Feeling
+	};
+	
+	public bool GetSenseEnabled (SenseType sense) {
+		switch(sense) {
+			case SenseType.Sight:
+				return SightGO.activeInHierarchy;
+			case SenseType.Hearing:
+				return HearingGO.activeInHierarchy;
+			case SenseType.Scent:
+				return ScentGO.activeInHierarchy;
+			case SenseType.Feeling:
+				return FeelingGO.activeInHierarchy;
+			default:
+				Debug.Log ("GetSenseEnabled: Invalid sense");
+				return false;
+		}
+	}
+	
+	public void SetSenseEnabled (SenseType sense, bool active) {
+		switch(sense) {
+			case SenseType.Sight:
+				SightGO.SetActive(active);
+				break;
+			case SenseType.Hearing:
+				HearingGO.SetActive(active);
+				break;
+			case SenseType.Scent:
+				ScentGO.SetActive(active);
+				break;
+			case SenseType.Feeling:
+				FeelingGO.SetActive(active);
+				break;
+			default:
+				Debug.Log ("SetSenseEnabled: Invalid sense");
+				break;
+		}
+	}
+}
