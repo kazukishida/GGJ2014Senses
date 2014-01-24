f925e861ec73ccbf0d97fabf6c27b0ceb2998e00
Nigel Rodriguez <anr.rod@gmail.com>
2014-01-24 15:35:34 +0000

    add sense cameras

---
+++ Assets/Scripts/Senses.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Senses : MonoBehaviour {
+	public GameObject SightGO;
+	public GameObject HearingGO;
+	public GameObject ScentGO;
+	public GameObject TouchGO;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
