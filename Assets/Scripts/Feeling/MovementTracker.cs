ad2e920e028268126ad9f8579c84a21f1e4485e7
Joshua Castaneda <thejosh223@gmail.com>
2014-01-26 00:34:42 +0800

    Added footsteps for touch mode

--- Assets/Scripts/Feeling/MovementTracker.cs
+++ Assets/Scripts/Feeling/MovementTracker.cs
@@ -5,7 +5,8 @@ public class MovementTracker : MonoBehaviour {
 
 	private Vector3 prevPosition;
 	private int nextPrefab = 0;
-	public float distanceBetweenFoots = 0.5f;
+	private float currentlyTravelled = 0;
+	public float distanceBetweenFoots = 4f;
 	public GameObject[] footstepsPrefab;
 
 	void Start () {
@@ -14,9 +15,21 @@ public class MovementTracker : MonoBehaviour {
 	
 	void Update () {
 		float distanceTravelled = Vector3.Distance(transform.position, prevPosition);
+		currentlyTravelled += distanceTravelled;
+		if (currentlyTravelled >= distanceBetweenFoots) {
+			currentlyTravelled -= distanceBetweenFoots;
+			DrawFoot();
+		}
+		prevPosition = transform.position;
 	}
 
 	public void DrawFoot() {
+		Vector3 v = transform.position;
+		v.y = 0;
+		v+= transform.forward * 0.4f;
+		GameObject g = Instantiate(footstepsPrefab[nextPrefab], v, 
+		                           Quaternion.Euler(new Vector3(90, transform.rotation.eulerAngles.y, 0))) as GameObject;
+		Destroy(g, 3f);
 		nextPrefab = (nextPrefab + 1) % footstepsPrefab.Length;
 	}
 }
