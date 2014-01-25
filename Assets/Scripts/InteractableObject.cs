225bc90d59688b11bc93968411b331f052018974
Joniel Ibasco <jlibasco@gmail.com>
2014-01-25 21:56:29 +0800

    Added support for InteractableObjects; InteractableObjects can either be carried or activated.

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableObject : MonoBehaviour {
+	public bool canCarry;
+	public bool canActivate;
+
+	public virtual bool activate() {}
+}
