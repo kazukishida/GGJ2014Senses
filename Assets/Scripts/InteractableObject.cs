3b8a112c55c5419641f27328b996c6b8880b2770
Joniel Ibasco <jlibasco@gmail.com>
2014-01-25 22:00:29 +0800

    Minor fix.

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -5,5 +5,7 @@ public class InteractableObject : MonoBehaviour {
 	public bool canCarry;
 	public bool canActivate;
 
-	public virtual bool activate() {}
+	public virtual bool activate() {
+		return true;
+	}
 }
