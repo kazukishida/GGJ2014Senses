f5c8d254a21dfab18cbfcb570052caf51a0b7b87
Joshua Castaneda <thejosh223@gmail.com>
2014-01-25 23:28:55 +0800

    Block Juggler + Sign Shit.

--- Assets/Scripts/Level/BlockJiggler.cs
+++ Assets/Scripts/Level/BlockJiggler.cs
@@ -6,10 +6,13 @@ public class BlockJiggler : MonoBehaviour {
 	void Start () {
 		GameObject[] g = GameObject.FindGameObjectsWithTag("Wall");
 		for (int i = 0; i < g.Length; i++) {
-			int dir = Random.Range(0, 3) == 0 ? 1 : -1;
-			Transform t = g[i].transform;
-			if (!Physics.Raycast(t.position, Vector3.up * dir, t.localScale.y)) {
-				t.position += new Vector3(0, t.localScale.y * 0.1f);
+			if (Random.Range(0, 2) == 0) {
+				int dir = Random.Range(0, 2) == 0 ? 1 : -1;
+				Transform t = g[i].transform;
+				if (!Physics.Raycast(t.position, Vector3.up * dir, t.localScale.y))
+					t.position += new Vector3(0, t.localScale.y * Random.Range(0f, 0.4f)) * dir;
+				else
+					t.position -= new Vector3(0, t.localScale.y * Random.Range(0f, 0.4f)) * dir;
 			}
 		}
 	}
