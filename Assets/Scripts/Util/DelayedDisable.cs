68ab46c133ff3099b4d9923fdd6245b280c7b52f
Joshua Castaneda <thejosh223@gmail.com>
2014-01-25 17:25:14 +0800

    Merged

---
+++ Assets/Scripts/Util/DelayedDisable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayedDisable : MonoBehaviour {
+
+	public float timeToDisable = 0;
+	
+	void Update () {
+		if (timeToDisable < Time.time)
+			gameObject.SetActive(false);
+	}
+
+	public void StartDelayedDisable(float duration) {
+		timeToDisable = Time.time + duration;
+	}
+}
